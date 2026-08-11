d70d7ae6475703e6eca7053dd05e6ddb670f4f82
weruh <werudennis19@gmail.com>
2026-08-11 13:30:31 +0300

    navbar error fix

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,16 +1,24 @@
-
-import { Link } from 'react-router';
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <>
-        <Navbar style={{display:'flex', backgroundColor:'red', }} >
-            <Link to="/home"> <img src="/src/assets/mawel2.png" alt="mawel.png" srcset="" /></Link>
-            <Link to="/dealership">Dealership</Link>
-            <Link to="/admin" >Admin</Link>
-        </Navbar>
-    </>
-  )
+    <nav style={{ display: "flex", alignItems: "center", justifyContent: "space-between",width: "100%", height: "10vh", backgroundColor: "skyblue",padding: "0 30px", boxSizing: "border-box",color:'black',}} >
+      <div>
+        <Link to="/">
+          <img src="/src/assets/mawel2.png" alt="Mawel Motors"  style={{ width: "80px", height: "80px", objectFit: "contain",}}/>
+        </Link>
+      </div>
+
+     
+      <div style={{display: "flex",  gap: "30px", }} >
+        <Link to="/dealership">Dealership</Link>
+      </div>
+
+      <div>
+        <Link to="/admin">Admin</Link>
+      </div>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
