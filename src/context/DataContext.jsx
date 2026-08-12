b5ea1087df1065bea84af9e8c1884ed7ace1029d
weruh <werudennis19@gmail.com>
2026-08-12 19:09:39 +0300

    implementation of usecontext

--- src/context/DataContext.jsx
+++ src/context/DataContext.jsx
@@ -1,4 +1,4 @@
-import { useState, createContext } from "react";
+import { useState, createContext , useContext} from "react";
 import dealershipdata from "../dealershipdata.js";
 
 
@@ -9,11 +9,23 @@ export const DataContext = createContext();
 export function DataProvider({ children }) {
   const [product, setProduct] = useState(dealershipdata);
 
+  const AddProduct = (newName, newDescription,newPrice,imageUrl) =>{
+   const newProduct = {
+    id: Date.now(),
+    name: newName,
+    description: newDescription,
+    price: newPrice,
+    image: imageUrl,
+   } 
+   setProduct((prevProduct) =>[...prevProduct, newProduct])
+  }
+
   return (
-    <DataContext.Provider value={{ product, setProduct }}>
+    <DataContext.Provider value={{ product, setProduct, AddProduct }}>
       {children}
     </DataContext.Provider>
   );
 }
+export const useData = () => useContext(DataContext);
 
 export default DataContext;
