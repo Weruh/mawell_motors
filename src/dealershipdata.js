7ee253d40dedfd1204bcba63cce5baeb3bcf07cc
Masha <ellymash45@gmail.com>
2026-08-14 20:42:27 +0300

    adjusted some items

--- src/dealershipdata.js
+++ src/dealershipdata.js
@@ -1,35 +1,41 @@
 const dealershipdata = [
   {
+    id: 1,
     name: "Aurora Sedan",
     description: "  A stylish and efficient sedan with advanced features and a comfortable interior.",
     price: "KSH. 2.4M",
     image: "src/assets/sedan.png",
   },
   {
+    id: 2,
     name: "Horizon SUV",
     description: "High-performance SUV with spacious interior and advanced safety features.",
     price: "KSH. 3.2M",
     image: "src/assets/suv.png",
   },
   {
+    id: 3,
     name: "Nimbus Coupe",
     description: "Nimble and stylish coupe with a sporty performance.",
     price: "KSH. 2.8M",
     image: "src/assets/coupe.png",
   },
   {
+    id: 4,
     name: "Cascade Hatchback",
     description: "Handy hatchback with excellent fuel efficiency and compact design.",
-    price: "KSH. 15M",
+    price: "Asking price: KSH. 15M",
     image: "src/assets/hatchback.png",
   },
   {
+    id: 5,
     name: "Zenith Pickup",
     description: "Outdoor-ready pickup truck with impressive towing capacity.",
     price: "KSH. 3.9M",
     image: "src/assets/pickup.png",
   },
   {
+    id: 6,
     name: "Breeze Convertible",
     description: "Open-top driving experience with a stylish design.",
     price: " KSH. 4.9M",
@@ -37,6 +43,7 @@ const dealershipdata = [
   },
 
    {
+    id: 7,
     name: "Subaru Outback",
     description: "Get ready for an adventure with the Subaru Outback, a versatile SUV that combines rugged capability with comfort and style.",
     price: " KSH. 4.2M",
@@ -44,6 +51,7 @@ const dealershipdata = [
   },
 
    {
+    id: 8,
     name: "BMW 3 Series",
     description: "With its sleek design, powerful engine, and advanced technology, the BMW 3 Series is a perfect blend of performance and luxury.",
     price: " KSH. 15M",
@@ -51,6 +59,7 @@ const dealershipdata = [
   },
 
    {
+    id: 9,
     name: "G Wagon",
     description: "A spacious and practical vehicle, the Wagon is perfect for families and those who need extra cargo space.",
     price: " KSH. 8M",
